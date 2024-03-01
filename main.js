4a929302f55d6386af448388c13457bcbb300123
ChanMor <morelos.franzchristian@gmail.com>
2024-03-01 15:29:41 +0800

    Created functions for validating password

--- main.js
+++ main.js
@@ -1,19 +1,84 @@
 const username = "Christian"
-const password1 = ""
-const password2 = ""
+const password1 = "p4sSw0rD"
+const password2 = "p4sSw0rD"
 
-matchPassword (password1, password2) {
+console.log(validatePassword(password1, password2))
 
+function matchPassword (password1, password2) {
+    if (password1 === password2) {
+        return true
+    } 
+    return false
 }
 
-lengthPassword (password) {
-
+function lengthPassword (password) {
+    if (password.length >= 8) {
+        return true
+    }
+    return false
 }
 
-strictPassword (password) {
-
+function numberedPassword (p) {
+    if (p >= '0' && p <= '9') {
+        return true
+    }
+    return false
 }
 
-validatePassword(password1, password2) {
+function upperedPassword (p) {
+    if (p === p.toUpperCase()) {
+        return true
+    }
+    return false
+}
 
+function loweredPassword (p) {
+    if (p === p.toLowerCase()) {
+       return true
+    }
+    return false
+}
+
+function strictPassword (password) {
+
+    const isValid = [false, false, false]
+
+    for (let i=0; i<password.length; i++) {
+
+        if (numberedPassword(password[i])) {
+            isValid[0] = true
+        }
+
+        if (upperedPassword(password[i])) {
+            isValid[1] = true
+        }
+        
+        if (loweredPassword(password[i])) {
+            isValid[2] = true
+        }
+    }
+
+    for (let i=0; i<isValid.length; i++) {
+        if (isValid[i] === false) {
+            return false
+        }
+    }
+    return true
+}
+
+function validatePassword(password1, password2) {
+
+    if (!matchPassword(password1, password2)) {
+        return false
+    }
+
+    if (!lengthPassword(password1) || !lengthPassword(password2)) {
+        return false
+    }
+
+    if (strictPassword(password1) && strictPassword(password2)){
+        return true
+    }
+
+    return false
 }
