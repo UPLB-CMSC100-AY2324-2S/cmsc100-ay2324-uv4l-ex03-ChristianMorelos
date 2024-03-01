48019bddeae0e509d7d62f8079a2cba4a8705492
ChristianMorelos <fdmorelos@up.edu.ph>
2024-03-01 14:30:53 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const username = "Christian"
+const password1 = ""
+const password2 = ""
+
+matchPassword (password1, password2) {
+
+}
+
+lengthPassword (password) {
+
+}
+
+strictPassword (password) {
+
+}
+
+validatePassword(password1, password2) {
+
+}
